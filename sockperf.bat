1731b0a13da340828e4f0472bb832ba5e2daa3c1
taiyuan <taiyuan.fang@gmail.com>
2024-06-15 16:35:28 +0800

    update bat

--- sockperf.bat
+++ sockperf.bat
@@ -2,14 +2,21 @@
 
 if not exist csv mkdir csv
 
-set /a arg_sz=%1
-set /a arg_hz=%2
-set /a arg_count=%3
-set /a count=1
+set    arg_host=127.0.0.1
+set    arg_port=11111
+set    arg_mps=100
+set    arg_time=60
+set    arg_msg_size=%1
+set /a arg_count=%2
 
-echo Size: %arg_sz%
-echo Hz:   %arg_hz%
-echo Loop: %arg_count%
+echo arg_host     : %arg_host%
+echo arg_port     : %arg_port%
+echo arg_mps      : %arg_mps%
+echo arg_time     : %arg_time%
+echo arg_msg_size : %arg_msg_size%
+echo arg_count    : %arg_count%
+
+set /a count=1
 
 :loop
     set pad=000000%count%
@@ -25,11 +32,11 @@ echo Loop: %arg_count%
     set date_y=%date:~6,4%
     set date_m=%date:~0,2%
     set date_d=%date:~3,2%
-    set filename=%date_y%%date_m%%date_d%_%time_h%%time_m%%time_s%_%arg_sz%_%arg_hz%_%seq%.csv
+    set filename=%date_y%%date_m%%date_d%_%time_h%%time_m%%time_s%_%arg_msg_size%_%seq%.csv
     rem echo filename=%filename%
 
     @echo on
-    bin\sockperf.exe pp -i 127.0.0.1 --tcp --full-log csv\%filename% --msg-size %arg_sz% --mps %arg_hz% -t 60
+    bin\sockperf.exe pp -i %arg_host% -p %arg_port% --tcp --mps %arg_mps% -t %arg_time% --msg-size %arg_msg_size% --full-log csv\%filename%
     @echo off
 
     set /a count+=1
